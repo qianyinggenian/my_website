50ee55edcbfa36b9deb71dba48a9e60903c5bd88
wangkangzhang <1368504885@qq.com>
2022-04-02 15:59:37 +0800

    更新

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,12 @@ module.exports = {
   outputDir: 'dist',
   // 开发服务器设置
   devServer: {
+    // 设置开发服务器的端口号
     port: 8080,
+    // 开服务器开启后，直接打开
     open: true
+  },
+  // 设置Webpack的原始配置
+  configureWebpack: {
   }
 }
